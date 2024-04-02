111f845f82f539ce231716f20e52117a24bf51c4
AmbroseX <rongkangxiong@idea.edu.cn>
2024-04-02 10:33:22 +0800

    79 部署脚本

--- scripts/env-118.sh
+++
@@ -1,8 +0,0 @@
-#安装环境 fastchat118
-
-conda create -n FastChat2 python=3.10
-
-conda acitvate FastChat2
-
-pip install -U pip
-
--- scripts/run_79_qwen.sh
+++ scripts/run_79_qwen.sh
@@ -1,23 +1,38 @@
+./scripts/stop_worker.sh
 # Qwen-1_8B-Chat
 # nohup env CUDA_VISIBLE_DEVICES=4,5,6,7 python3 ./fastchat/serve/model_worker.py --num-gpus 4 --host 192.168.80.34 --port 22002 --worker http://192.168.80.34:22002 --controller-address http://192.168.80.34:22001 --model-name "Qwen-1_8B-Chat" --model-path /platform_tech/models/Qwen-1_8B-Chat  --max-gpu-memory '80Gib' --limit-worker-concurrency 20 > ./logs/worker.log 2>&1 &
 
 # vllm方式启动
 # 1个任务可以跑35tokens/s
 # 一张A100 80G可以跑 接近50个并行，1810tokens/s
-# echo "load Model: Qwen-1_8B-Chat" > ./logs/worker_Qwen-1_8B-Chat.log
-# nohup env CUDA_VISIBLE_DEVICES=3 python3 ./fastchat/serve/vllm_worker.py \
-#     --controller-address http://192.168.80.34:22001 \
-#     --host 192.168.80.33 --port 22002  \
-#     --worker-address http://192.168.80.33:22002 \
-#     --model-path /platform_tech/models/Qwen-1_8B-Chat  \
-#     --model-name "Qwen-1_8B-Chat" \
-#     --num-gpus 1 \
-#     --gpu_memory_utilization 0.65 \
-#     --max-model-len 4096 --worker-use-ray \
-#     --enforce-eager \
-#     --limit-worker-concurrency 20 \
-#     >> ./logs/worker_Qwen-1_8B-Chat.log 2>&1 &
+echo "load Model: Qwen1.5-4B-Chat" > ./logs/worker_79_Qwen1.5-4B-Chat.log
+nohup env CUDA_VISIBLE_DEVICES=1,2 python3 -m fastchat.serve.vllm_worker \
+    --controller-address http://192.168.80.34:22001 \
+    --host 192.168.190.79 --port 22002  \
+    --worker-address http://192.168.190.79:22002 \
+    --model-path /platform_tech/models/Qwen1.5-4B-Chat  \
+    --model-name "Qwen1.5-4B-Chat" \
+    --num-gpus 2 \
+    --gpu_memory_utilization 0.85 \
+    --max-model-len 8192 --worker-use-ray \
+    --enforce-eager \
+    --limit-worker-concurrency 20 \
+    >> ./logs/worker_79_Qwen1.5-4B-Chat.log 2>&1 &
 
+
+echo "load Model: Qwen1.5-1.8B-Chat" > ./logs/worker_79_Qwen1.5-1.8B-Chat.log
+nohup env CUDA_VISIBLE_DEVICES=3 python3 -m fastchat.serve.vllm_worker \
+    --controller-address http://192.168.80.34:22001 \
+    --host 192.168.190.79 --port 22003 \
+    --worker-address http://192.168.190.79:22003 \
+    --model-path /platform_tech/models/Qwen1.5-1.8B-Chat  \
+    --model-name "Qwen1.5-1.8B-Chat" \
+    --num-gpus 1 \
+    --gpu_memory_utilization 0.85 \
+    --max-model-len 8192 --worker-use-ray \
+    --enforce-eager \
+    --limit-worker-concurrency 20 \
+    >> ./logs/worker_79_Qwen1.5-1.8B-Chat.log 2>&1 &
 
 # Qwen-7B-Chat
 # vllm方式启动
@@ -36,22 +51,23 @@
 
 
 
-# Qwen-14B-Chat
+# Qwen1.5-14B-Chat
+# 40个注意头
 # vllm方式启动
-echo "load Model: Qwen-14B-Chat"  > ./logs/worker_79_Qwen-14B-Chat.log
-nohup env CUDA_VISIBLE_DEVICES=1,3 python3  ./fastchat/serve/vllm_worker.py \
+echo "load Model: Qwen1.5-14B-Chat"  > ./logs/worker_79_Qwen1.5-14B-Chat.log
+nohup env CUDA_VISIBLE_DEVICES=4,5,6,7 python3  -m fastchat.serve.vllm_worker \
     --controller-address http://192.168.80.34:22001 \
     --host 192.168.190.79 --port 22010 \
     --worker-address http://192.168.190.79:22010   \
-    --model-path /platform_tech/xiongrongkang/models/Qwen-14B-Chat \
-    --model-name "Qwen-14B-Chat" \
-    --num-gpus 2 \
-    --gpu_memory_utilization 0.95 \
-    --max-model-len 8192 --worker-use-ray \
+    --model-path /platform_tech/models/Qwen1.5-14B-Chat \
+    --model-name "Qwen1.5-14B-Chat" \
+    --num-gpus 4 \
+    --gpu_memory_utilization 0.8 \
+    --max-model-len 32768 --worker-use-ray \
     --limit-worker-concurrency 20 \
     --tokenizer-mode "auto" \
     --enforce-eager \
-    >> ./logs/worker_79_Qwen-14B-Chat.log 2>&1 &
+    >> ./logs/worker_79_Qwen1.5-14B-Chat.log 2>&1 &
 
 # Qwen-14B-Chat
 # echo "load Model: Qwen-14B-Chat"  > ./logs/worker_Qwen-14B-Chat.log
