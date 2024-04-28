19b7169516a73b288d278a27a646623efc1e1ff9
AmbroseX <rongkangxiong@idea.edu.cn>
2024-04-28 11:54:52 +0800

    worker 0.2.2

---
+++ docker/All/build-docker.sh
@@ -0,0 +1,18 @@
+export version=0.1
+# 步骤1: 追加内容到 .dockerignore
+# echo "model_logs/" >> .dockerignore
+# echo "logs/" >> .dockerignore
+# echo "wheel_cache/" >> .dockerignore
+# 步骤2: 执行Docker构建或其他操作
+
+sudo docker build  --build-arg BUILD_DATE=$(date +%Y-%m-%d:%H:%M:%S) -t fastchat-all:${version} -f ./docker/All/Dockerfile.all .
+
+# 步骤3: 删除 .dockerignore 中的 "wheel_cache/" 行
+# sed -i '/model_logs\//d' .dockerignore
+# sed -i '/logs\//d' .dockerignore
+# sed -i "/wheel_cache\//d" .dockerignore
+
+
+# 对于需要 wheel_cache 的构建，确保 .dockerignore 不包含对它的忽略
+# 可以是删除该行，或完全重新生成 .dockerignore 文件
+
--- docker/Worker/run-docker-worker.sh
+++ docker/Worker/run-docker-worker.sh
@@ -1,18 +1,22 @@
-# ./scripts/stop_worker.sh
-
+export version=0.2.2
+sudo docker stop fastchat-worker && sudo docker rm fastchat-worker
+# Controller地址
+export FASTCHAT_CONTROLLER_PORT=22001
+export FASTCHAT_CONTROLLER_ADDRESS='192.168.190.79'
+# worker端口
 export FASTCHAT_WORKER_ADDRESS='192.168.190.79'
-export FASTCHAT_WORKER_PORT=22002
-export FASTCHAT_WORKER_MODEL_NAMES='qinshu_extract'
-export docker_name='qinshu_extract'
-export model_path='/platform_tech/xiongrongkang/checkpoint/qinshu/qinshu_extract_all_v4_qwen1_5_4b'
+export FASTCHAT_WORKER_PORT=22003
+export FASTCHAT_WORKER_MODEL_NAMES='fastchat-worker'
+export docker_name='fastchat-worker'
+export model_path='/platform_tech/models/Qwen1.5-1.8B-Chat'
 
 sudo docker run --rm -d --security-opt seccomp:unconfined -e OPENBLAS_NUM_THREADS=1 \
-    --gpus '"device=7"' -e NUM_GPUS=1 \
+    --gpus '"device=6"' -e NUM_GPUS=1 \
     -e TZ=Asia/Shanghai \
     -e NVIDIA_DRIVER_CAPABILITIES="compute,utility" \
     --cpus="10" --memory="20g" --shm-size="8gb" \
-    -e FASTCHAT_CONTROLLER_ADDRESS='192.168.80.34' \
-    -e FASTCHAT_CONTROLLER_PORT='22001' \
+    -e FASTCHAT_CONTROLLER_ADDRESS=${FASTCHAT_CONTROLLER_ADDRESS} \
+    -e FASTCHAT_CONTROLLER_PORT=${FASTCHAT_CONTROLLER_PORT} \
     -e FASTCHAT_WORKER_ADDRESS=${FASTCHAT_WORKER_ADDRESS} \
     -e FASTCHAT_WORKER_PORT=${FASTCHAT_WORKER_PORT} \
     -p ${FASTCHAT_WORKER_PORT}:${FASTCHAT_WORKER_PORT}\
@@ -33,4 +37,4 @@ sudo docker run --rm -d --security-opt seccomp:unconfined -e OPENBLAS_NUM_THREAD
     -v ${model_path}:/app/models \
     -v /platform_tech/xiongrongkang/FastChat/model_logs:/app/model_logs \
     --network="host" \
-    --name ${docker_name} fastchat-worker:0.1
+    --name ${docker_name} fastchat-worker:${version}
